4cb89d4730f6476238a9f6faac3b34c4eb9411d0
Terrance <notterrancehuang@gmail.com>
2022-05-10 18:44:52 -0400

    changed class name

--- src/easy/TwoSum.java
+++ src/easy/TwoSum.java
@@ -1,6 +1,6 @@
 package easy;
 
-public class Solution {
+public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
