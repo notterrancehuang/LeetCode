6a8f5592c94289a4f2a78e15d0104b1d9f882e4a
Terrance <notterrancehuang@gmail.com>
2022-05-10 18:40:25 -0400

    fixed TwoSum

--- src/easy/TwoSum.java
+++ src/easy/TwoSum.java
@@ -1,14 +1,17 @@
 package easy;
 
-public class TwoSum {
+public class Solution {
     public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[]{i, map.get(complement)};
             }
         }
-        return new int[2];
+        return null;
     }
 }
