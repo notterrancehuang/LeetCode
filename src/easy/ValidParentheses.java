613842f07c5e52038d7ee45082eeab4c324bc237
Terrance <notterrancehuang@gmail.com>
2022-05-16 16:23:25 -0400

    ValidParentheses

---
+++ src/easy/ValidParentheses.java
@@ -0,0 +1,30 @@
+package easy;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        Stack<Character> leftParen = new Stack<>();
+        for (char c : s.toCharArray()) {
+            // opening parentheses
+            if (c == '(' || c == '[' || c == '{') {
+                leftParen.push(c);
+            }
+            
+            // closing parentheses
+            else if (c == ')' && !leftParen.isEmpty() && leftParen.peek() == '(') {
+                leftParen.pop();
+            } else if (c == ']' && !leftParen.isEmpty() && leftParen.peek() == '[') {
+                leftParen.pop();
+            } else if (c == '}' && !leftParen.isEmpty() && leftParen.peek() == '{') {
+                leftParen.pop();
+            }
+
+            // not a valid symbol
+            else {
+                return false;
+            }
+        }
+        return leftParen.isEmpty();
+    }
+}
