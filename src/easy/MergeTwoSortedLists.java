62acc4090bc50e1856d76aaf93f8e7a3f4e41d50
Terrance <notterrancehuang@gmail.com>
2022-05-19 18:12:52 -0400

    updated MergeTwoSortedLists

--- src/easy/MergeTwoSortedLists.java
+++ src/easy/MergeTwoSortedLists.java
@@ -10,15 +10,9 @@ public class ListNode {
 
 class MergeTwoSortedLists {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if (list1 == null) {
-            return list2;
-        }
-        if (list2 == null) {
-            return list1;
-        }
-        ListNode result = new ListNode();
-        ListNode tail = result;
-
+        ListNode head = new ListNode();
+        ListNode tail = head;
+        
         while (list1 != null && list2 != null) {
             if (list1.val < list2.val) {
                 tail.next = new ListNode(list1.val);
@@ -34,13 +28,13 @@ class MergeTwoSortedLists {
             tail.next = new ListNode(list1.val);
             list1 = list1.next;
             tail = tail.next;
-        } 
+        }
         while (list2 != null) {
             tail.next = new ListNode(list2.val);
             list2 = list2.next;
             tail = tail.next;
         }
 
-        return result.next;
+        return head.next;
     }
 }
