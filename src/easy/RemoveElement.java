8762ce177fba95d53f2eec273f9258efb222e44d
Terrance <notterrancehuang@gmail.com>
2022-05-26 11:30:30 -0400

    RemoveElement

---
+++ src/easy/RemoveElement.java
@@ -0,0 +1,14 @@
+package easy;
+
+public class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        int pointer = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[pointer++] = nums[i];
+            }
+        }
+
+        return pointer;
+    }
+}
