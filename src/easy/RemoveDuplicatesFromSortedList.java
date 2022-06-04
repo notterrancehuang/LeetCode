f4116aaf7b19a0557d5c01334ab7742e65364bea
Terrance <notterrancehuang@gmail.com>
2022-06-03 20:12:39 -0400

    Remove duplicates from sorted list

---
+++ src/easy/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,34 @@
+package easy;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class RemoveDuplicatesFromSortedList {
+    public static ListNode deleteDuplicates(ListNode head) {
+        ListNode curr = head;
+
+        while (curr != null && curr.next != null) {
+            if (curr.val == curr.next.val) {
+                curr.next = curr.next.next;
+            } else {
+                curr = curr.next;
+            }
+        }
+
+        return head;
+    }
+}
