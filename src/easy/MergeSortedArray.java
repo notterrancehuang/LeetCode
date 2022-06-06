1ca7e48f88594ff1392b544b18ac7b585cc1a77b
Terrance <notterrancehuang@gmail.com>
2022-06-06 13:13:31 -0400

    MergeSortedArray

---
+++ src/easy/MergeSortedArray.java
@@ -0,0 +1,23 @@
+package easy;
+
+public class MergeSortedArray {
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int size = m + n - 1;
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] >= nums2[j]) {
+                nums1[size] = nums1[i--];
+            } else {
+                nums1[size] = nums2[j--];
+            }
+            size--;
+        }
+        while (i >= 0) {
+            nums1[size--] = nums1[i--];
+        }
+        while (j >= 0) {
+            nums1[size--] = nums2[j--];
+        }
+    }
+}
