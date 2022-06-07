27e2ca7775aa2715a9432c405331d83a964f0abb
Terrance <notterrancehuang@gmail.com>
2022-06-07 15:45:16 -0400

    Symmetric tree

---
+++ src/easy/SymmetricTree.java
@@ -0,0 +1,54 @@
+package easy;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class SymmetricTree {
+    public static boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        return isSymmetric(root.left, root.right);
+    }
+
+    public static boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+
+        if (left == null || right == null) {
+            return false;
+        }
+
+        if (left.val != right.val) {
+            return false;
+        }
+
+        if (!isSymmetric(left.left, right.right)) {
+            return false;
+        }
+
+        if (!isSymmetric(left.right, right.left)) {
+            return false;
+        }
+
+        return true;
+    }
+}
