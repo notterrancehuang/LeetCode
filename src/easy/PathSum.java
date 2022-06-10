793e5158280b80b70a502a528e8991b1099fb400
Terrance <notterrancehuang@gmail.com>
2022-06-10 13:14:58 -0400

    Path sum

---
+++ src/easy/PathSum.java
@@ -0,0 +1,15 @@
+package easy;
+
+import utils.TreeNode;
+
+public class PathSum {
+    public static boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return root.val == targetSum;
+        }
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+    }
+}
