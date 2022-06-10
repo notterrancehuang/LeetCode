56a144bf0d945f53959166681ecd69252a8555c7
Terrance <notterrancehuang@gmail.com>
2022-06-10 13:27:42 -0400

    Pascal's triangle

---
+++ src/easy/PascalsTriangle.java
@@ -0,0 +1,20 @@
+package easy;
+
+import java.util.List;
+import java.util.Arrays;
+
+public class PascalsTriangle {
+    public static List<List<Integer>> generate(int numRows) {
+        int[][] pascal = new int[numRows][];
+        for (int i = 0; i < numRows; i++) {
+            int[] row = new int[i + 1];
+            row[0] = 1;
+            row[i] = 1;
+            for (int j = 1; j < i; j++) {
+                row[j] = pascal[i - 1][j - 1] + pascal[i - 1][j];
+            }
+            pascal[i] = row;
+        }
+        return (List) Arrays.asList(pascal);
+    }
+}
