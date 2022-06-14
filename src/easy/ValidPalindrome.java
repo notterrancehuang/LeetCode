15064ab7169aa7427af6a1811bbba1a024d9dfa4
Terrance <notterrancehuang@gmail.com>
2022-06-14 13:43:52 -0400

    added comments

--- src/easy/ValidPalindrome.java
+++ src/easy/ValidPalindrome.java
@@ -2,6 +2,7 @@ package easy;
 
 public class ValidPalindrome {
     public static boolean isPalindrome(String s) {
+        // removes all non-alphanumeric characters and converts to lowercase
         s = s.toLowerCase().replaceAll("[^A-Za-z0-9]", "");
         int start = 0;
         int end = s.length() - 1;
