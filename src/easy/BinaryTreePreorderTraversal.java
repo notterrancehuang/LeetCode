25616d37428a0ea662c8c1b384ebab0709740d2b
Terrance <notterrancehuang@gmail.com>
2022-06-16 12:29:58 -0400

    Binary tree preorder traversal

---
+++ src/easy/BinaryTreePreorderTraversal.java
@@ -0,0 +1,24 @@
+package easy;
+
+import utils.TreeNode;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class Solution {
+    public static List<Integer> preorderTraversal(TreeNode root) {
+        // preorder: root, left, right
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        while (!stack.isEmpty() || root != null) {
+            while (root != null) {
+                list.add(root.val);
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            root = root.right;
+        }
+        return list;
+    }
+}
