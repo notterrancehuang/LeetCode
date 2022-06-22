d4cfc77e432d6429e3e40a2b72f1aad4990b8e21
Terrance <notterrancehuang@gmail.com>
2022-06-22 15:45:29 -0400

    Two sum

--- src/easy/TwoSum.java
+++ src/easy/TwoSum.java
@@ -4,16 +4,17 @@ import java.util.HashMap;
 
 public class TwoSum {
     public static int[] twoSum(int[] nums, int target) {
+        // num, index of complement
         HashMap<Integer, Integer> map = new HashMap<>();
+        int[] result = new int[2];
         for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], i);
-        }
-        for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-            if (map.containsKey(complement) && map.get(complement) != i) {
-                return new int[]{i, map.get(complement)};
+            if (!map.containsKey(nums[i])) {
+                map.put(target - nums[i], i);
+            } else {
+                result[0] = i;
+                result[1] = map.get(nums[i]);
             }
         }
-        return null;
+        return result;
     }
 }
