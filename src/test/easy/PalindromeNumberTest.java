526dcca345a5028c9628b92768a06a5663ab058b
Terrance <notterrancehuang@gmail.com>
2022-06-22 16:31:35 -0400

    Palindrome number test cases

--- src/test/easy/PalindromeNumberTest.java
+++ src/test/easy/PalindromeNumberTest.java
@@ -1,4 +1,22 @@
+package test.easy;
+
+import main.easy.PalindromeNumber;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class PalindromeNumberTest {
-  
+    @org.junit.jupiter.api.Test
+    public void isPalindrome121() {
+        assertTrue(PalindromeNumber.isPalindrome(121));
+    }
+
+    @org.junit.jupiter.api.Test
+    void isPalindromeNegative121() {
+        assertFalse(PalindromeNumber.isPalindrome(-121));
+    }
+
+    @org.junit.jupiter.api.Test
+    void isPalindrome10() {
+        assertFalse(PalindromeNumber.isPalindrome(10));
+    }
 }
