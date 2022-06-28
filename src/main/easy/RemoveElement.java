9d7fc8935892c90a6cd41d1a838a30a69b380312
Terrance <notterrancehuang@gmail.com>
2022-06-28 14:36:59 -0400

    Remove element

--- src/main/easy/RemoveElement.java
+++ src/main/easy/RemoveElement.java
@@ -2,13 +2,12 @@ package main.easy;
 
 public class RemoveElement {
     public static int removeElement(int[] nums, int val) {
-        int pointer = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != val) {
-                nums[pointer++] = nums[i];
+        int pointer = 0; 
+        for (int num : nums) {
+            if (num != val) {
+                nums[pointer++] = num;
             }
         }
-
         return pointer;
     }
 }
