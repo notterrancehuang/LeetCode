42279b378513162c2aa8b272d36f27a69833d687
Terrance <notterrancehuang@gmail.com>
2022-06-29 12:24:35 -0400

    Search insert position

--- src/main/easy/SearchInsertPosition.java
+++ src/main/easy/SearchInsertPosition.java
@@ -2,13 +2,12 @@ package main.easy;
 
 public class SearchInsertPosition {
     public static int searchInsert(int[] nums, int target) {
-        int insert = 0; // pointer where to insert
+        int insert = 0;
         for (int i = 0; i < nums.length; i++) {
             if (target > nums[i]) {
                 insert++;
             }
         }
-
         return insert;
     }
 }
