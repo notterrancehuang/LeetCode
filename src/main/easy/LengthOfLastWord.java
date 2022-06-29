56e77219e5210367b72e8905549d027dbc82755c
Terrance <notterrancehuang@gmail.com>
2022-06-29 12:27:23 -0400

    Length of last word

--- src/main/easy/LengthOfLastWord.java
+++ src/main/easy/LengthOfLastWord.java
@@ -2,8 +2,7 @@ package main.easy;
 
 public class LengthOfLastWord {
     public static int lengthOfLastWord(String s) {
-        String[] strArray = s.split(" ");
-        int numWords = strArray.length;
-        return strArray[numWords - 1].length();
+        String[] stringArray = s.split(" ");
+        return stringArray[stringArray.length - 1].length();
     }
 }
