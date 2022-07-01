14e1b71ff34b1cdca31f9e139be0f0539f4c67db
Terrance <notterrancehuang@gmail.com>
2022-07-01 12:41:49 -0400

    Sqrt(x)

--- src/main/easy/SqrtX.java
+++ src/main/easy/SqrtX.java
@@ -2,29 +2,28 @@ package main.easy;
 
 public class SqrtX {
     public static int mySqrt(int x) {
-        if (x <= 1) {
-            return x;
-        }
-
         int left = 1;
         int right = x / 2;
-        int mid;
+        long square;
         int answer = 0;
-        long temp;
 
+        if (x < 2) {
+            return x;
+        }
+        
         while (left <= right) {
-            mid = (left + (right - left) / 2);
-            temp = (long) mid * (long) mid;
-            if (temp == x) {
+            int mid = left + (right - left) / 2;
+            square = (long) mid * (long) mid;
+            if (square == x) {
                 return mid;
-            }
-            if (temp < x) {
+            } else if (square < x) {
                 answer = mid;
                 left = mid + 1;
             } else {
                 right = mid - 1;
             }
         }
+
         return answer;
     }
 }
